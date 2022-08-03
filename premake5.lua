bd56d905a7325f62e5ab26aaf74ab13e7f3348f0
Yannik Hodel <yannik.hodel@gmail.com>
2022-08-03 22:04:03 +0200

    Setup premake

---
+++ premake5.lua
@@ -0,0 +1,75 @@
+workspace "Mini2DEngine"
+	architecture "x64"
+
+	configurations { "Debug", "Release", "Dist" }
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/int/" .. outputdir .. "/%{prj.name}")
+
+	files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+
+	includedirs { "Mini2DEngine/src" }
+
+	links { "Mini2DEngine" }
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines { "ME_PLATFORM_WINDOWS" }
+
+	filter "configurations:Debug"
+		defines "ME_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ME_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ME_DIST"
+		optimize "On"
+
+project "Mini2DEngine"
+	location "Mini2DEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin/int/" .. outputdir .. "/%{prj.name}")
+
+	files { "%{prj.name}/src/**.h", "%{prj.name}/src/**.cpp" }
+
+	--includedirs { "%{prj.name}/vendor/spdlog/include" }
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines { "ME_PLATFORM_WINDOWS", "ME_BUILD_DLL" }
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ME_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ME_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ME_DIST"
+		optimize "On"
