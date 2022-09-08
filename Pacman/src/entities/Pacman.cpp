2d971e9d2e093ba2d739c88fa7bfc62ff8d9ff1a
Yannik Hodel <yannik.hodel@gmail.com>
2022-09-08 15:58:13 +0200

    Improve pacman movement with input buffer

--- Pacman/src/entities/Pacman.cpp
+++ Pacman/src/entities/Pacman.cpp
@@ -5,43 +5,69 @@ namespace PacmanGame
 {
 
 	Pacman::Pacman(float x, float y, MiniEngine::Ref<MiniEngine::Texture> texture) : MiniEngine::Entity("Pacman", x, y, 20.0f, 20.0f, 0.0f, texture),
-		direction(0.0f, 0.0f), prevDirection(0.0f, 0.0f) {}
+		direction(0.0f, 0.0f), prevDirection(0.0f, 0.0f), bufferedInput(MiniEngine::Key::SPACE) {}
 
 	void Pacman::OnUpdate(float deltaTime)
 	{
+		int posX = GetPosition().x - 10;
+		int posY = GetPosition().y - 10;
 		justChangedDirection = false;
 		//Pacman Controls
-		if (MiniEngine::Input::IsKeyPressed(MiniEngine::Key::W) && direction != glm::vec2(0.0f, 1.0f))
+		if (MiniEngine::Input::IsKeyPressed(MiniEngine::Key::W) || bufferedInput == MiniEngine::Key::W && direction != glm::vec2(0.0f, 1.0f))
 		{
-			prevDirection = direction;
-			direction = { 0.0f, 1.0f };
-			prevRotation = GetRotation();
-			SetRotation(90.0f);
-			justChangedDirection = true;
+			if (posX % 20 == 0)
+			{
+				prevDirection = direction;
+				direction = { 0.0f, 1.0f };
+				prevRotation = GetRotation();
+				SetRotation(90.0f);
+				justChangedDirection = true;
+				bufferedInput = MiniEngine::Key::SPACE;
+			}
+			else if (bufferedInput != MiniEngine::Key::W)
+				bufferedInput = MiniEngine::Key::W;
 		}
-		else if (MiniEngine::Input::IsKeyPressed(MiniEngine::Key::A) && direction != glm::vec2(-1.0f, 0.0f))
+		else if (MiniEngine::Input::IsKeyPressed(MiniEngine::Key::A) || bufferedInput == MiniEngine::Key::A && direction != glm::vec2(-1.0f, 0.0f))
 		{
-			prevDirection = direction;
-			direction = { -1.0f, 0.0f };
-			prevRotation = GetRotation();
-			SetRotation(180.0f);
-			justChangedDirection = true;
+			if (posY % 20 == 0)
+			{
+				prevDirection = direction;
+				direction = { -1.0f, 0.0f };
+				prevRotation = GetRotation();
+				SetRotation(180.0f);
+				justChangedDirection = true;
+				bufferedInput = MiniEngine::Key::SPACE;
+			}
+			else if (bufferedInput != MiniEngine::Key::A)
+				bufferedInput = MiniEngine::Key::A;
 		}
-		else if (MiniEngine::Input::IsKeyPressed(MiniEngine::Key::S) && direction != glm::vec2(0.0f, -1.0f))
+		else if (MiniEngine::Input::IsKeyPressed(MiniEngine::Key::S) || bufferedInput == MiniEngine::Key::S && direction != glm::vec2(0.0f, -1.0f))
 		{
-			prevDirection = direction;
-			direction = { 0.0f, -1.0f };
-			prevRotation = GetRotation();
-			SetRotation(270.0f);
-			justChangedDirection = true;
+			if (posX % 20 == 0)
+			{
+				prevDirection = direction;
+				direction = { 0.0f, -1.0f };
+				prevRotation = GetRotation();
+				SetRotation(270.0f);
+				justChangedDirection = true;
+				bufferedInput = MiniEngine::Key::SPACE;
+			}
+			else if (bufferedInput != MiniEngine::Key::S)
+				bufferedInput = MiniEngine::Key::S;
 		}
-		else if (MiniEngine::Input::IsKeyPressed(MiniEngine::Key::D) && direction != glm::vec2(1.0f, 0.0f))
+		else if (MiniEngine::Input::IsKeyPressed(MiniEngine::Key::D) || bufferedInput == MiniEngine::Key::D && direction != glm::vec2(1.0f, 0.0f))
 		{
-			prevDirection = direction;
-			direction = { 1.0f, 0.0f };
-			prevRotation = GetRotation();
-			SetRotation(0.0f);
-			justChangedDirection = true;
+			if (posY % 20 == 0)
+			{
+				prevDirection = direction;
+				direction = { 1.0f, 0.0f };
+				prevRotation = GetRotation();
+				SetRotation(0.0f);
+				justChangedDirection = true;
+				bufferedInput = MiniEngine::Key::SPACE;
+			}
+			else if (bufferedInput != MiniEngine::Key::D)
+				bufferedInput = MiniEngine::Key::D;
 		}
 
 		glm::vec2& pos = this->GetPosition();
--- Pacman/src/entities/Pacman.h
+++ Pacman/src/entities/Pacman.h
@@ -17,6 +17,7 @@ namespace PacmanGame
         const float speed = 100.0f;
         glm::vec2 direction;
         glm::vec2 prevDirection;
+        MiniEngine::KeyCode bufferedInput;
         float prevRotation;
         bool justChangedDirection = false;
     };
